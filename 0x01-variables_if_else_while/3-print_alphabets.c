87f0ad0b167a746a9f992071dcf3dd7374d95fee
khaldi505 <habetlbrika69@gmail.com>
2019-09-19 11:49:09 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char ch;
+ch = 'a';
+while (ch <= 'z')
+{
+putchar (ch);
+ch = ch + 1;
+}
+ch = 'A';
+while (ch <= 'Z')
+{
+putchar (ch);
+ch = ch + 1;
+}
+putchar ('\n');
+return (0);
+}

c4aec8164c6c0782912731ec3bc2579ef0f7ab78
khaldi505 <habetlbrika69@gmail.com>
2019-09-19 13:08:32 -0700

    print comb2

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * putchar a-z
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a, b;
+for (a = '0'; a <= '9'; a++)
+{
+for (b = '0'; b <= '9'; b++)
+{
+putchar(a);
+putchar(b);
+if (!((a == '9') && (b == '9')))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}

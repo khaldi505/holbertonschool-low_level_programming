90040e711289397cbbd27f2fb2d7f3ca0646ed17
khaldi505 <habetlbrika69@gmail.com>
2019-09-24 12:13:37 +0000

    _putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,13 +1,13 @@
-#include <holberton.h>
-#include <stdio.h>
-int main (void)
+#include "holberton.h"
+
+int main(void)
 {
-  int c;
-  char string[]= "Holberton";
-  for ( c = 0; c < 9 ; c,,)
-    {
-     _ putchar(string[c]);
-    }
-  _putchar('\n')
-    return (0);
+int c;
+char string[] = "Holberton";
+for (c = 0; c < 9 ; c++)
+{
+_putchar(string[c]);
+}
+_putchar('\n');
+return (0);
 }

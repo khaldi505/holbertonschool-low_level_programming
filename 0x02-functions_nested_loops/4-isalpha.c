d2a9963df61ce5749be4d570cd90260c7570e91f
khaldi505 <habetlbrika69@gmail.com>
2019-09-24 18:16:09 +0000

    isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+ /**
+  *_isalpha : check the code if c letter is lowercase or uppercase
+  *Returns 1
+  */
+int _isalpha(int c)
+{
+if (((c >= 97) && (c <= 122)) || ((c >= 65) && (c <= 90)))
+return (1);
+else
+return (0);
+}

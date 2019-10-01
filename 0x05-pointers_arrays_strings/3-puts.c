29532068eaa6098858a29a3555d85157d58e3424
khaldi505 <habetlbrika69@gmail.com>
2019-10-01 22:21:47 +0100

    akhdem

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,18 @@
 #include "holberton.h"
+
 /**
 * _puts - print a string
 *@str: string to print
 *Return: nothing
 */
+
 void _puts(char *str)
 {
-int n;
-n = 0;
-while (*(str + n) != '\0')
+int i = 0;
+while (*(str + i) != '\0')
 {
-_putchar(*(str + n));
-n += 1;
+_putchar(*(str + i));
+i++;
 }
 _putchar('\n');
 }

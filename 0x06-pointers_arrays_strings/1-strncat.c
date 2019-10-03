be3624adb1a3b4265b7b5243265d27e48867573b
khaldi505 <habetlbrika69@gmail.com>
2019-10-03 18:19:09 +0100

    UwU

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,12 @@
+char *_strncat(char *dest, char *src, int n)
+{
+int i,k;
+i = 0;
+k = 0;
+while (dest[i] != '\0')
+i++;
+for (k = 0; k < n && src[k] != '\0'; k++, i++)
+dest[i] = src[k];
+dest[i] = '\0';
+return (dest);
+}

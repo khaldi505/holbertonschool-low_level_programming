99158786e4fabede150dd1838755a7ac388fe44b
khaldi505 <habetlbrika69@gmail.com>
2019-10-06 15:29:42 +0100

    mrigel deglaaa

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,19 +1,12 @@
 #include <stdio.h>
 /**
-<<<<<<< HEAD
  * main - the main function
  *
  * Return: Always 0
  */
-=======
-*main - the main function.
-*
-* Return: Always 0
-**/
->>>>>>> af072b8aafeb3abb661962321a4ab7ad874a355c
 int main(void)
 {
-int numb;
+char numb;
 for (numb = '0'; numb <= '9'; numb++)
 putchar (numb);
 putchar ('\n');

90bf47b7fcba12bd68d4c41c91cbef610987be70
khaldi505 <habetlbrika69@gmail.com>
2019-10-07 13:11:47 +0100

    never back down ya rojla

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,24 +1,24 @@
 #include "holberton.h"
 /**
- * print_sign - entry point
+ * print_sign - function that prints the sign of a number.
  * @n : number to check
- * Return: int
+ * Return: int and prints a sign of a number
  */
 int print_sign(int n)
 {
-if (n > 0)
+if (n > '0')
 {
-_putchar('+');
+_putchar ('+');
 return (1);
 }
-else if (n < 0)
-{
-_putchar('-');
-return (-1);
-}
-else
+if (n == 0)
 {
 _putchar('0');
 return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
 }
 }

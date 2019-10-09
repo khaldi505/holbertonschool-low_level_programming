50a57c3485b988582e9e7f9d59883f35cb137f82
khaldi505 <habetlbrika69@gmail.com>
2019-10-09 23:57:17 +0100

    bader rojla

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,18 +5,15 @@
  *
  */
 void _puts_recursion(char *s)
+
 {
-int i;
-i = 0;
-_putchar(s[i]);
-i++;
-if (s[i] == '\0')
+if (*s == '\0')
 {
 _putchar('\n');
-
 }
-if (s[i] != '\0')
+else
 {
-_puts_recursion(s + i);
+_putchar(*s);
+_puts_recursion((s + 1));
 }
 }

7933a56edc726e43588f2f9e55ddb84f4d1a7ed3
khaldi505 <habetlbrika69@gmail.com>
2019-10-13 21:27:44 +0100

    args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,12 +5,12 @@
  *@argv: An array of size argc
  *Return: 0
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 int i;
-for(i = 0; i < argc; i++)
+for (i = 0; i < argc; i++)
 {
 printf("%s\n", argv[i]);
 }
- return (0);
+return (0);
 }

b5d63e39f76643a5e45b9c4531d6d2f41bc2294d
khaldi505 <habetlbrika69@gmail.com>
2019-10-13 21:10:57 +0100

    red bull is cool

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-int main(int argc,char *argv[]__attribute__((unused)))
+/**
+ *main - entry point
+ *@argc: The size of the argv array
+ *@argv: An array containing the program command line arguments
+ *Return: 0
+ */
+int main(int argc, char *argv[]__attribute__((unused)))
 {
-
-printf("%d\n", argc-1);
+printf("%d\n", argc - 1);
 return (0);
 }

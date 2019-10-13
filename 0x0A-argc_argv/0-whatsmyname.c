d331c2f32f2c078bc883d8ff01353369920609c1
khaldi505 <habetlbrika69@gmail.com>
2019-10-13 20:10:25 +0100

    where is my mind

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - the main function "entry point"
+ *@argc: int type
+ *@argv: char array
+ *Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[argc - 1]);
+return (0);
+}

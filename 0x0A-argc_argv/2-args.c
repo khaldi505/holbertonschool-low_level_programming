b1da400499262e2842b05c16341d83ebb8023c2e
khaldi505 <habetlbrika69@gmail.com>
2019-10-13 21:26:13 +0100

    args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *@argc: The size of the argv array
+ *@argv: An array of size argc
+ *Return: 0
+ */
+int main(int argc, char* argv[])
+{
+int i;
+for(i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+ return (0);
+}

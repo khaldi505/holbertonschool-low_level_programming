2a4a90c1cde40a0ea765bcc6299350668bb19b2e
khaldi505 <habetlbrika69@gmail.com>
2019-10-23 17:46:52 +0100

    arara

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
 #ifndef HEADER_H
 #define HEADER_H
 void print_name(char *name, void (*f)(char *));
-#define size_t unsigned int
+#define size_t int
 void array_iterator(int *array, size_t size, void (*action)(int));
 #endif

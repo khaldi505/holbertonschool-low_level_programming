f09ab1bb022db64af23c048fbb253e4bb840d4ba
khaldi505 <habetlbrika69@gmail.com>
2019-10-23 19:19:18 +0100

    ss

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,7 @@
 #ifndef HEADER_H
 #define HEADER_H
+#include <stddef.h>
+int int_index(int *array, int size, int (*cmp)(int));
 void print_name(char *name, void (*f)(char *));
 #define size_t int
 void array_iterator(int *array, size_t size, void (*action)(int));

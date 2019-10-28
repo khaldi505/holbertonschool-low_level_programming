152b1ab4101c433e27bfb1f97be983569c0a8cf7
khaldi505 <habetlbrika69@gmail.com>
2019-10-28 19:12:13 +0100

    save arezzo files

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,11 +14,11 @@ char *s;
 va_start(ap, n);
 for (i = 0; i < n; i++)
 {
-s = va_arg(ap, char*);
 if (s == NULL)
 {
-printf("nil");
+printf("(nil)");
 }
+s = va_arg(ap, char *);
 printf("%s", s);
 if (i < n - 1 && separator != NULL)
 printf("%s", separator);

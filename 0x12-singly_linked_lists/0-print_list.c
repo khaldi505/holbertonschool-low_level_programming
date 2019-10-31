c7add974041c870ca89c19c5db2fd41187bc104e
khaldi505 <habetlbrika69@gmail.com>
2019-10-31 18:19:11 +0100

    guess who complete the first task

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+/**
+*print_list - prints all the elements of a list_t list.
+*@h: pointer to the node
+*Return: the number of nodes
+*/
+size_t print_list(const list_t *h)
+{
+size_t i = 0;
+const list_t *tmpp;
+tmpp = h;
+if (tmpp->str == NULL)
+{
+printf("[0] (nil)\n");
+tmpp = tmpp->next;
+i++;
+}
+while (tmpp != NULL)
+{
+printf("[%d]%s\n", tmpp->len, tmpp->str);
+tmpp = tmpp->next;
+i++;
+}
+return (i);
+}

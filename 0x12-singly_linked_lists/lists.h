e4602231a7f75b551edc9b49fb8f41c9750f4a3d
khaldi505 <habetlbrika69@gmail.com>
2019-11-01 10:35:02 +0100

    labes

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,6 +2,7 @@
 #define LIST_S
 #include <stdio.h>
 #include <stddef.h>
+#include <stdlib.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -11,14 +12,11 @@
  * Description: singly linked list node structure
  * for Holberton project
  */
-#include <stddef.h>
-#include <stdlib.h>
-#include <stdio.h>
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);

82cd71beffaa6d9443a2ced629f149fe09a8516e
khaldi505 <habetlbrika69@gmail.com>
2019-11-05 21:27:18 +0100

    sum

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+*get_nodeint_at_index - returns the nth node of a linked list
+*@head: pointer to the first element of the list (the list)
+*@index: uns int
+*Return: nth node. If node does not exist, retrs NULL.//Aladin Bensassi
+*/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+unsigned int indexx;
+for (indexx = 0; indexx < index && head != NULL; indexx++)
+{
+head = head->next;
+}
+return (head);
+}

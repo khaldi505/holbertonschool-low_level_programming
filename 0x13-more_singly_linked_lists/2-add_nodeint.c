085eb2fc52da28122efad6bd1f8546975f27b7bc
khaldi505 <habetlbrika69@gmail.com>
2019-11-05 12:50:21 +0100

    add node 3aad

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,11 +9,13 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 listint_t *tmp;
-if ((head == NULL) || (tmp == NULL))
+if (head == NULL)
 {
 return (NULL);
 }
 tmp = malloc(sizeof(listint_t));
+if (tmp == NULL)
+return (NULL);
 tmp->n = n;
 tmp->next = *head;
 *head = tmp;

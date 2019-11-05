d5137a695b5f0769f521fc7a91496694ae71abb0
khaldi505 <habetlbrika69@gmail.com>
2019-11-05 21:29:46 +0100

    sum

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -2,8 +2,8 @@
 /**
 *get_nodeint_at_index - returns the nth node of a linked list
 *@head: pointer to the first element of the list (the list)
-*@index: uns int
-*Return: nth of the list
+*@index: unsigned int
+*Return: the sum of all the data (n) of a listint_t linked list
 */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {

96caad15534dc8a41f3b7e9d4602b5b5262059c7
khaldi505 <habetlbrika69@gmail.com>
2019-11-16 16:21:35 +0100

    read

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+*read_textfile -  reads a text file and prints it to the POSIX standard output.
+*@filename: pointer to the file name
+*@letters: how many char in the file to print
+*Return: ssize_t
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+unsigned int r, w;
+int i = open(filename, O_RDONLY);
+if (!i)
+{
+return (-1);
+close(i);
+}
+char *buf;
+buf = malloc(sizeof(char) * letters);
+r = read(i, buf, letters);
+if (!r)
+{
+return (-1);
+}
+w = write(1, buf, letters);
+close(w);
+return (letters);
+}

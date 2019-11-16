ad00206a8b329f7644e014fab49e987ccc2e03f7
khaldi505 <habetlbrika69@gmail.com>
2019-11-16 21:47:31 +0100

    fixed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,16 +7,16 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-int i, w;
+int i, w, c = 0;
 if (filename == NULL)
-{
 return (-1);
-}
 i = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 if (i == -1)
 return (0);
-w = write(i, text_content, sizeof(text_content));
-if (w == -1)
+while (text_content[c] != '\0')
+c++;
+w = write(i, text_content, c);
+if ((w == -1) && (w != c))
 return (-1);
 close(i);
 return (1);

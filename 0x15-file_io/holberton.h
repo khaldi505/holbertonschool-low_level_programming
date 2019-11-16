bed6f16592232c038795c59f37733f4a68e0c6d8
khaldi505 <habetlbrika69@gmail.com>
2019-11-16 23:30:29 +0100

    fixed

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -6,6 +6,7 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 #include <fcntl.h>
+int append_text_to_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 #endif

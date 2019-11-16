46555dde9a7eb67c281155ae914a4ef4ce599850
khaldi505 <habetlbrika69@gmail.com>
2019-11-16 23:37:22 +0100

    fixed

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,8 +1,9 @@
 #include "holberton.h"
 /**
-*
-*
-*
+*append_text_to_file - append text to file func
+*@filename: pointer to the file name
+*@text_content: text content on the file
+*Return: 1 or -1 in case if it fails
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
@@ -16,7 +17,7 @@ return (-1);
 }
 if (text_content == NULL)
 return (1);
-while(text_content[co] != '\0')
+while (text_content[co] != '\0')
 co++;
 w = write(i, text_content, co);
 if (w == -1)

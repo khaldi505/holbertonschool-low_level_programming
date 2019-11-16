5a7cccd376bf767a026d22d5662e4088d9225a72
khaldi505 <habetlbrika69@gmail.com>
2019-11-16 18:29:54 +0100

    all

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,25 +7,26 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+ssize_t r, w;
 if (filename == NULL)
 {
 return(0);
 }
-ssize_t r, w;
 int i = open(filename, O_RDONLY);
 if (i == -1)
-{
 return (0);
-}
 char *buf;
+
 buf = malloc(sizeof(char) * letters);
 r = read(i, buf, letters);
-close(r);
 if (r == -1)
 {
+free(buf);
 return (0);
 }
-w = write(STDOUT_FILENO, buf, r + 1);
+w = write(STDOUT_FILENO, buf, r);
+if (w == -1 || w != r)
+return (0);
 close(i);
 return (w);
 }

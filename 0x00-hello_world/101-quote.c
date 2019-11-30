9948d4ba86104427191ef5a9ee9ab33c3b66e226
khaldi505 <habetlbrika69@gmail.com>
2019-11-29 17:48:06 -0800

    fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,13 +7,6 @@
 */
 int main(void)
 {
-int i;
-i = 0;
-char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-while (str[i] != '\0')
-{
-i++;
-}
-write(2, str, i);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" , 61);
 return (1);
 }

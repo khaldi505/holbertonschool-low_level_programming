6bad2a9d519d2e04926b331604c31aafd5371d69
khaldi505 <habetlbrika69@gmail.com>
2019-12-01 15:13:25 -0800

    fixed

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,9 +6,9 @@
  */
 int print_sign(int n)
 {
-if (n > '0')
+if (n > 0)
 {
-_putchar ('+');
+_putchar('+');
 return (1);
 }
 if (n == 0)
@@ -17,8 +17,6 @@ _putchar('0');
 return (0);
 }
 else
-{
 _putchar('-');
 return (-1);
-}
 }

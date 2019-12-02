a899f7ca2122cce4a3f3f1cd34db9694cde239ff
khaldi505 <habetlbrika69@gmail.com>
2019-12-01 18:03:11 -0800

    fixed

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,21 +2,19 @@
 /**
 *print_rev -  prints a string, in reverse, followed by a new line.
 *@s: a pointer to the first character of the string
-*Return: void 
+*Return: void
 */
 void print_rev(char *s)
 {
 int len = 0;
-int lenull;
-lenull = 0;
+int lenull = 0;
 while (s[lenull] != '\0')
 lenull++;
 while (s[len] != '\0')
 {
-_putchar(s[lenull]);
+_putchar(s[lenull - 1]);
 len++;
 lenull--;
 }
-_putchar(*s);
 _putchar('\n');
 }

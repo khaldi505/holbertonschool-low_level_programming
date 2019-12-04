fafa3b463542985f2e663c87d99263af70f31d06
khaldi505 <habetlbrika69@gmail.com>
2019-12-03 17:01:56 -0800

    the s in my name stands smrt

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,7 +6,11 @@
  */
 int _strlen_recursion(char *s)
 {
-if (!*s)
-return (0);
-return (1 + _strlen_recursion(s + 1));
+int res = 0;
+if (*s != '\0')
+{
+res = _strlen_recursion(s + 1);
+res++;
+}
+return (res);
 }

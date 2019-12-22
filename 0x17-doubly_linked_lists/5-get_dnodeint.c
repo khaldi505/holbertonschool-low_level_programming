026665b425c47499e58db769d515ef7abb935f08
khaldi505 <habetlbrika69@gmail.com>
2019-12-22 22:21:01 +0100

    the nth kekha node

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+*get_dnodeint_at_index - returns the nth node of a dlistint_t linked list.
+*@head: the head of the list
+*@index: the index
+*Return: a pointer to the nth node
+*/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+dlistint_t *current_node;
+unsigned int nth = 0;
+while (head != NULL)
+{
+if (nth + 1 == index)
+{
+current_node = head->next;
+break;
+}
+nth++;
+head = head->next;
+}
+return (current_node);
+}

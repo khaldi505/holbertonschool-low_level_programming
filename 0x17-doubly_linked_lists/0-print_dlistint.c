d0486f145768ca280ce20604295dce36e6e16ee1
khaldi505 <habetlbrika69@gmail.com>
2019-12-22 15:15:43 +0100

    re do soko

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,9 @@
 #include "lists.h"
-
+/**
+*print_dlistint - print all the element in a doubly linked list
+*@h: the head of the list
+*Return: the number of the elements on a linked list
+*/
 size_t print_dlistint(const dlistint_t *h)
 {
 const dlistint_t *temp = h;
--- 0x17-doubly_linked_lists/README.md
+++
@@ -1,1 +0,0 @@
-__ 0x17. C - Doubly linked lists __

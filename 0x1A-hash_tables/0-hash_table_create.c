db8cf423aecd73ca913b7a58548215886e01a043
khaldi505 <habetlbrika69@gmail.com>
2020-01-19 16:22:14 +0100

    fixed the size of the array

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,15 +6,15 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-if (!(size))
-return (NULL);
 hash_table_t *new;
-new = (hash_table_t *) malloc(sizeof(hash_table_t));
+new = malloc(sizeof(hash_table_t));
 if (new == NULL)
 return (NULL);
+if (size == 0)
+return (NULL);
 new->size = size;
-new->array = malloc(sizeof(hash_node_t **));
-if (new->array == NULL))
+new->array = malloc(sizeof(hash_node_t *) * size);
+if (new->array == NULL)
 {
 free(new);
 return (NULL);

ae6ec5610cc2df6997c13976493ca94b422adb66
khaldi505 <habetlbrika69@gmail.com>
2020-01-19 15:48:23 +0100

    valgrind fixed

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,7 +13,7 @@ new = malloc(sizeof(hash_table_t));
 if (new == NULL)
 return (NULL);
 new->size = size;
-new->array = (hash_node_t **) malloc(sizeof(hash_table_t));
+new->array = (hash_node_t **) malloc(sizeof(hash_table_t) * size);
 if (new->array == NULL)
 {
 free(new);

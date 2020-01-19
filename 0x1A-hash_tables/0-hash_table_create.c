37fc2b77dd414796d0673068a4a0e64b48ec188c
khaldi505 <habetlbrika69@gmail.com>
2020-01-19 15:07:08 +0100

    why i got b this dumb

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,8 +1,8 @@
 #include "hash_tables.h"
 /**
-*hash_table_create - a function that creates a hash table.
-*@size: the size of the array.
-*Return: Returns a pointer to the newly created hash table.
+* hash_table_create - a function that creates a hash table.
+* @size: the size of the array.
+* Return: Returns a pointer to the newly created hash table.
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {

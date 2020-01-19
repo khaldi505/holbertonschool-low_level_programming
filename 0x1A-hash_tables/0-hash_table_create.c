46471844fe9149bf568d08f7c7c4b4bc709b46b3
khaldi505 <habetlbrika69@gmail.com>
2020-01-19 16:10:53 +0100

    changed

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,12 +9,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 if (!(size))
 return (NULL);
 hash_table_t *new;
-new = malloc(sizeof(hash_table_t));
+new = (hash_table_t *) malloc(sizeof(hash_table_t));
 if (new == NULL)
 return (NULL);
 new->size = size;
 new->array = malloc(sizeof(hash_node_t **));
-if (!(new->array))
+if (new->array == NULL))
 {
 free(new);
 return (NULL);

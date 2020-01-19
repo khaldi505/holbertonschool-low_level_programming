c861421ba83f11fbc814ebd73b4e28066733de87
khaldi505 <habetlbrika69@gmail.com>
2020-01-19 15:47:43 +0100

    valgrind fixed

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,20 +1,20 @@
 #include "hash_tables.h"
 /**
-* hash_table_create - a function that creates a hash table.
-* @size: the size of the array.
-* Return: Returns a pointer to the newly created hash table.
+*hash_table_create - creates a hash table.
+*@size: the size of the array.
+*Return: a pointer to the newly created hash table.
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-hash_table_t *new;
 if (size == 0)
 return (NULL);
+hash_table_t *new;
 new = malloc(sizeof(hash_table_t));
 if (new == NULL)
 return (NULL);
 new->size = size;
-new->array = malloc(sizeof(hash_table_t) * size);
-if (!(new->array))
+new->array = (hash_node_t **) malloc(sizeof(hash_table_t));
+if (new->array == NULL)
 {
 free(new);
 return (NULL);

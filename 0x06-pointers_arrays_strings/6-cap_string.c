c223925d8a1a0ef000fc979d4bd5ab6c8346250e
khaldi505 <habetlbrika69@gmail.com>
2020-05-10 18:40:48 +0200

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ *cap_string - capitalizes the first letter in a world
+ *@s: string
+ *Return: a string
+ */
+char *cap_string(char *s)
+{
+int counter = 0;
+
+while (s[counter] != '\0')
+{
+if ((s[counter - 1]) && (s[counter - 1] == ' ' ||
+s[counter - 1] == '\n' || s[counter - 1] == '\t'))
+if (s[counter] >= 'a' || (s[counter] <= 'z' && !(s[counter] < 'a')))
+{
+s[counter] -= 32;
+}
+counter++;
+}
+
+return (s);
+}

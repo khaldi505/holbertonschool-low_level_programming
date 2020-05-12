2b504c4c6e4db4d345dda96c36f3e32a7b2743b3
khaldi505 <habetlbrika69@gmail.com>
2020-05-12 05:58:13 +0200

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * leet - finds a sepsifc letter and replace it by an intger
+ *@s: string
+ *Return: a string
+ */
+char *leet(char *s)
+{
+int counter, sec = 0;
+
+char uppercase[] = {'a', 'e', 'o', 't', 'l'};
+char lowercase[] = {'A', 'E', 'O', 'T', 'L'};
+int numbers[] = {4, 3, 0, 7, 1};
+
+for (counter = 0; s[counter] != '\0'; counter++)
+{
+for (sec = 0; numbers[sec] != '\0'; sec++)
+{
+if ((s[counter] == lowercase[sec]) || (s[counter] == uppercase[sec]))
+{
+s[counter] = numbers[sec];
+}
+}
+}
+return (s);
+}

5d897ba79abc96c66df71a6ef99867d5a91e86bb
khaldi505 <habetlbrika69@gmail.com>
2020-07-08 11:58:17 -0700

    fc

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,15 +9,13 @@
 void print_diagsums(int *a, int size)
 {
 int x = 0;
-int i = 0;
+int i;
 int result = 0;
 int sum = a[x];
 for (i = 0; i < size; i++)
 {
-sum += a[x + size + 1];
+sum += *(a + i * size + i);
 result += *(a + (i * size) + (size - 1 - i));
-x += size + 1;
 }
-printf("%d, ", sum);
-printf("%d\n", result);
+printf("%d, %d\n", sum, result);
 }

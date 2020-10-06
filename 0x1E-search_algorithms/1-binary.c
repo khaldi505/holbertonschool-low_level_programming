aba63bbad39759177e8210edc6d2d628d25ed4ea
khaldi505 <habetlbrika69@gmail.com>
2020-10-06 03:36:02 +0000

    this took me literally the whole day :)

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,55 @@
+#include "search_algos.h"
+/**
+ *print_array - a function will print from n th element until to the n th element
+ *@from: from where you want to start printing 
+ * @to: to when you want to stop
+ *Return: nothing
+ */
+void print_array(int *array, size_t from, size_t to)
+{
+	printf("Searching in array: ");
+	while (from < to)
+	{
+		if (from + 1 == to)
+			printf("%d ", array[from]);
+		else
+			printf("%d, ", array[from]);
+		from++;
+	} printf("\n");
+}
+
+/**
+*binary_search - a function that do binary search on array.
+*@array: the array we're going to fetch.
+*@size: size of the array.
+*@value: the value we're looking for.
+*Return: the index of the Value.
+*/
+int binary_search(int *array, size_t size, int value)
+{
+size_t L = 0;
+size_t R = size - 1;
+size_t m;
+if(!array || !value || L > R)
+return (-1);
+print_array(array, L, R);
+	while (L < R)
+	{	
+		m = (L + R) / 2;
+		if (array[m] < value)
+			{	
+				L = m + 1;
+print_array(array, L, R + 1);
+
+			}
+		else if (array[m] > value)
+			{
+				R = m - 1;
+print_array(array, L, R + 1);
+			}
+		else
+			return (m);
+		
+	}
+return (-1);
+}
